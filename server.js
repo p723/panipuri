17fb1545a11be120c5a64daff08b5d872854817b
p723 <68102093+p723@users.noreply.github.com>
2021-06-30 19:04:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,18 +11,7 @@ app.listen(PORT, () => {
         console.log(`listening on port ${PORT}`)
 })
 
-//set template engine
-app.use(expressLayouts)
-app.use(express.static(path.join(__dirname, 'resources')));
-
-// Set 'views' directory for any views 
-// being rendered res.render()
-app.set('views', path.join(__dirname, 'views'));
-
-// Set view engine as EJS
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
-        res.render('home')
+        res.send('home')
 } )
