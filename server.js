8178acd65709bc26463a4c1c052939d4df708b44
p723 <pranavbhatkar789@gmail.com>
2021-07-02 12:34:22 +0530

    second

--- server.js
+++ server.js
@@ -7,16 +7,31 @@ const expressLayouts = require("express-ejs-layouts")
 
 const PORT = process.env.PORT || 3000
 
+// Set template engine
+app.use(expressLayouts)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+
+app.use(express.static('public'))
 app.get('/', (req, res) => {
         res.render('home')
+} )
+
+app.get('/cart', (req, res) => {
+        res.render('customers/cart')
+} )
+
+app.get('/login', (req, res) => {
+        res.render('auth/login')
+} )
+
+app.get('/register', (req, res) => {
+        res.render('auth/register')
 } )
 
 app.listen(PORT, () => {
         console.log(`listening on port ${PORT}`)
 })
 
-// Set template engine
-app.use(expressLayouts)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
 
