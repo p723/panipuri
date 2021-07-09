7e2ad78fdfcfb14c64f5f2485af7e0a2233fa8b1
p723 <pranavbhatkar789@gmail.com>
2021-07-09 10:03:51 +0530

    admin order not completed

---
+++ app/http/controllers/customers/orderController.js
@@ -0,0 +1,48 @@
+const Order = require('../../../models/order')
+const moment = require('moment')
+
+function orderController () {
+         return {
+                  async index(req, res) {
+                           const order = await Order.find({
+                                    customerId: req.user._id
+                           }, null,
+                                    {
+                                             sort: {
+                                                      'createdAt': -1
+                                             }
+                                    })
+                           res.render('customers/orders', {
+                                    orders: order, moment: moment
+                           })
+                  },
+                  store(req, res) {
+                           const {
+                                    phone,
+                                    address
+                           } = req.body
+                           if (!phone || !address) {
+                                    req.flash('error', 'All fields are required')
+                                    req.flash('phone', phone)
+                                    req.flash('address', address)
+                                    return res.redirect('/cart')
+                           }
+                           const order = new Order({
+                                    customerId: req.user._id,
+                                    items: req.session.cart.items,
+                                    phone,
+                                    address
+                           })
+
+                           order.save().then(result => {
+                                    req.flash('success', 'Order placed')
+                                    delete req.session.cart
+                                    return res.redirect('/customer-orders')
+                           }).catch(err => {
+                                    req.flash('error', 'Something went Wrong..!')
+                                    return res.redirect('/cart')
+                           })
+                  }
+         }
+}
+module.exports = orderController
--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,10 @@
 const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const orderController = require('../app/http/controllers/customers/orderController')
+const AdminOrderController = require('../app/http/controllers/admin/orderController')
 const guest = require('../app/http/middleware/guest')
+const auth = require('../app/http/middleware/auth')
 
 
 function initRoutes(app) {
@@ -16,5 +19,12 @@ function initRoutes(app) {
          app.post('/update-cart', cartController().update)
          app.post('/delete-cart', cartController().deleteItem)
          app.post('/add-cart', cartController().additem)
+
+         //costomer routes
+         app.post('/orders', auth, orderController().store)
+         app.get('/customer/orders', auth, orderController().index)
+
+         // Admin routes
+         app.get('/admin/orders', auth, AdminOrderController().index)
 }
 module.exports = initRoutes
