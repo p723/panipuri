fbe241601dc015b597649bd761aa252ce3e71f79
p723 <68102093+p723@users.noreply.github.com>
2021-09-30 09:40:19 +0530

    0

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -36,7 +36,7 @@ export function initAdmin(socket) {
                     <p>${ order._id }</p>
                     <div>${ renderItems(order.items) }</div>
                 </td>
-                <td class="border px-4 py-2">${ order.customerId }</td>
+                <td class="border px-4 py-2">${ order.customerId.name }</td>
                 <td class="border px-4 py-2">${ order.address }</td>
                 <td class="border px-4 py-2">
                     <div class="inline-block relative w-64">
